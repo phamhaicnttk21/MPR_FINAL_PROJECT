daba69bb71aed00bfd70bc6f1f3fe1131161e4a5
hothuyhien <hothuyhien2412@gmail.com>
2024-04-25 21:26:23 +0700

    write and read data from firestore

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,10 +1,13 @@
 import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyBJcDc0YgUvhcSuqHvjaLAJG3pFomubuOI',
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID || 'mpr-c0c8a',
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
@@ -13,4 +16,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+  
+const db = getFirestore(app);
+
 export default app;
+export {db};
