bfc3980e9bfa206530fee5f28e8634f1cb7473cb
hoangsonasdf <108865470+hoangsonasdf@users.noreply.github.com>
2024-04-28 09:57:17 +0700

    show age options

--- screens/Aspect.js
+++ screens/Aspect.js
@@ -1,225 +1,250 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Animated, Dimensions } from 'react-native';
-import { dummyData } from '../dummyData'; 
+import { View, Text, TouchableOpacity, StyleSheet, Animated, Dimensions, FlatList } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons'; // Assuming you're using Expo for vector icons
+import { dummyData } from '../dummyData';
+
 const { height } = Dimensions.get('window');
 
 const Aspect = () => {
- const [showMenu, setShowMenu] = useState(null);
- const [selectedOptions, setSelectedOptions] = useState([]);
- const [loading, setLoading] = useState(true);
- const menuAnimation = useRef(new Animated.Value(-height / 2)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showMenu, setShowMenu] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const menuAnimation = useRef(new Animated.Value(-height / 2)).current;
 
- useEffect(() => {
-  setLoading(false);
- }, []);
+  const toggleMenu = (menuType) => {
+    if (showMenu === menuType) {
+      hideMenu();
+    } else {
+      showMenuAnimation(menuType);
+    }
+  };
 
- const toggleMenu = (menuType) => {
-  if (showMenu === menuType) {
-   hideMenu();
-  } else {
-   showMenuAnimation(menuType);
-  }
- };
+  const showMenuAnimation = (menuType) => {
+    setShowMenu(menuType);
+    Animated.timing(menuAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
- const showMenuAnimation = (menuType) => {
-  setShowMenu(menuType);
-  Animated.timing(menuAnimation, {
-   toValue: 0,
-   duration: 300,
-   useNativeDriver: true,
-  }).start();
- };
+  const hideMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: -height / 2,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      setShowMenu(null);
+    });
+  };
 
- const hideMenu = () => {
-  Animated.timing(menuAnimation, {
-   toValue: -height / 2,
-   duration: 300,
-   useNativeDriver: true,
-  }).start(() => {
-   setShowMenu(null);
-  });
- };
-
- const renderMenuContent = () => {
-  let options = [];
-  switch (showMenu) {
-   case 'career':
-    options = dummyData.career;
-    break;
-   case 'social':
-    options = dummyData.social;
-    break;
-   case 'finance':
-    options = dummyData.finance;
-    break;
-   case 'activities':
-    options = dummyData.activities;
-    break;
-   default:
-    return null;
-  }
-  return (
-   <View style={styles.menuContent}>
-    {options.map((option) => (
-     <TouchableOpacity key={option.id} >
-      <View style={styles.menuOptionContainer}>
-       <Text style={styles.menuOptionText}>
-        {option.option}
-       </Text>
-       {selectedOptions.includes(option.id) && (
-        <TouchableOpacity onPress={() => removeOption(option.id)}>
-         <FontAwesome name="times-circle" size={20} color="red" />
-        </TouchableOpacity>
-       )}
+  const renderMenuContent = () => {
+    let options = [];
+    switch (showMenu) {
+      case 'career':
+        options = dummyData.career;
+        break;
+      case 'social':
+        options = dummyData.social;
+        break;
+      case 'finance':
+        options = dummyData.finance;
+        break;
+      case 'activities':
+        options = dummyData.activities;
+        break;
+      default:
+        return null;
+    }
+    return (
+      <View style={styles.menuContent}>
+        {options.map((option) => (
+          <TouchableOpacity key={option.id} >
+            <View style={styles.menuOptionContainer}>
+              <Text style={styles.menuOptionText}>
+                {option.option}
+              </Text>
+              {selectedOptions.includes(option.id) && (
+                <TouchableOpacity onPress={() => removeOption(option.id)}>
+                  <FontAwesome name="times-circle" size={20} color="red" />
+                </TouchableOpacity>
+              )}
+            </View>
+          </TouchableOpacity>
+        ))}
       </View>
-     </TouchableOpacity>
-    ))}
-   </View>
-  );
- };
+    );
+  };
 
- const removeOption = (id) => {
-  setSelectedOptions(selectedOptions.filter(optionId => optionId !== id));
- };
- const showAllOptions = () => {
-  setShowMenu('all');
-  Animated.timing(menuAnimation, {
-   toValue: 0,
-   duration: 300,
-   useNativeDriver: true,
-  }).start();
- };
+  const removeOption = (id) => {
+    setSelectedOptions(selectedOptions.filter(optionId => optionId !== id));
+  };
 
- return (
-  <View style={styles.wrap}>
-   <View style={styles.aspectOverview}>
-    <View style={styles.itemContainer}>
-     <TouchableOpacity style={styles.career} onPress={() => toggleMenu('career')}>
-      <Text style={styles.careerText}>Career</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style={styles.social} onPress={() => toggleMenu('social')}>
-      <Text style={styles.socialText}>Social</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style={styles.finance} onPress={() => toggleMenu('finance')}>
-      <Text style={styles.financeText}>Finance</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style={styles.activities} onPress={() => toggleMenu('activities')}>
-      <Text style={styles.activitiesText}>Activities</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style={styles.allOptionsButton} onPress={showAllOptions}>
-      <Text style={styles.allOptionsText}>Show All</Text>
-     </TouchableOpacity>
+  const showAllOptions = () => {
+    setShowMenu('all');
+    Animated.timing(menuAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex(currentIndex => (currentIndex + 1) % dummyData.ageoptions.length);
+    }, 12 * 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <View style={styles.wrap}>
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Age: {dummyData.ageoptions[currentIndex].age}</Text>
+        <FlatList
+          data={dummyData.ageoptions[currentIndex].activities}
+          renderItem={({ item: activity }) => (
+            <TouchableOpacity onPress={() => handleOptionPress(activity.option)}>
+              <View style={{ marginLeft: 20 }}>
+                <Text>{activity.option}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(activity, index) => index.toString()}
+        />
+      </View>
+      <View style={styles.aspectOverview}>
+        <View style={styles.itemContainer}>
+          <TouchableOpacity style={styles.career} onPress={() => toggleMenu('career')}>
+            <Text style={styles.careerText}>Career</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.social} onPress={() => toggleMenu('social')}>
+            <Text style={styles.socialText}>Social</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.finance} onPress={() => toggleMenu('finance')}>
+            <Text style={styles.financeText}>Finance</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.activities} onPress={() => toggleMenu('activities')}>
+            <Text style={styles.activitiesText}>Activities</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.allOptionsButton} onPress={showAllOptions}>
+            <Text style={styles.allOptionsText}>Show All</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Animated.View style={[styles.menuContainer, { transform: [{ translateY: menuAnimation }] }]}>
+        {showMenu === 'all' ? (
+          <View style={styles.menuContent}>
+            {dummyData.career.map((option) => (
+              <Text key={option.id} style={styles.menuOptionText}>
+                {option.option}
+              </Text>
+            ))}
+            {dummyData.social.map((option) => (
+              <Text key={option.id} style={styles.menuOptionText}>
+                {option.option}
+              </Text>
+            ))}
+            {dummyData.finance.map((option) => (
+              <Text key={option.id} style={styles.menuOptionText}>
+                {option.option}
+              </Text>
+            ))}
+            {dummyData.activities.map((option) => (
+              <Text key={option.id} style={styles.menuOptionText}>
+                {option.option}
+              </Text>
+            ))}
+          </View>
+        ) : renderMenuContent()}
+      </Animated.View>
     </View>
-   </View>
-   <Animated.View style={[styles.menuContainer, { transform: [{ translateY: menuAnimation }] }]}>
-    {showMenu === 'all' ? (
-     <View style={styles.menuContent}>
-      {dummyData.career.map((option) => (
-       <Text key={option.id} style={styles.menuOptionText}>
-        {option.option}
-       </Text>
-      ))}
-      {dummyData.social.map((option) => (
-       <Text key={option.id} style={styles.menuOptionText}>
-        {option.option}
-       </Text>
-      ))}
-      {dummyData.finance.map((option) => (
-       <Text key={option.id} style={styles.menuOptionText}>
-        {option.option}
-       </Text>
-      ))}
-      {dummyData.activities.map((option) => (
-       <Text key={option.id} style={styles.menuOptionText}>
-        {option.option}
-       </Text>
-      ))}
-     </View>
-    ) : renderMenuContent()}
-   </Animated.View>
-  </View>
- );
+  );
 };
 
 const styles = StyleSheet.create({
- wrap: {
-  width: "100%",
-  height: "100%",
-  backgroundColor: '#FEFBF6',
- },
- aspectOverview: {
-  width: "100%",
-  height: "10%",
-  backgroundColor: "#1679AB",
-  marginTop: "110%",
- },
- itemContainer: {
-  flexDirection: 'row',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  flex: 1,
- },
- career: {
-  backgroundColor: 'gray',
-  padding: 10,
-  borderRadius: 5,
- },
- careerText: {
-  color: 'white',
- },
- social: {
-  backgroundColor: 'gray',
-  padding: 10,
-  borderRadius: 5,
- },
- socialText: {
-  color: 'white',
- },
- finance: {
-  backgroundColor: 'gray',
-  padding: 10,
-  borderRadius: 5,
- },
- financeText: {
-  color: 'white',
- },
- activities: {
-  backgroundColor: 'gray',
-  padding: 10,
-  borderRadius: 5,
- },
- activitiesText: {
-  color: 'white',
- },
- allOptionsButton: {
-  backgroundColor: 'gray',
-  padding: 10,
-  borderRadius: 5,
- },
- allOptionsText: {
-  color: 'white',
- },
- menuContainer: {
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  right: 0,
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  justifyContent: 'center',
-  alignItems: 'center',
-  overflow: 'hidden',
-  height: height / 2,
- },
- menuContent: {
-  backgroundColor: 'white',
-  padding: 16,
-  borderRadius: 8,
- },
- menuOptionText: {
-  fontSize: 16,
-  marginBottom: 8,
- },
+  wrap: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: '#FEFBF6',
+  },
+  aspectOverview: {
+    width: "100%",
+    height: "10%",
+    backgroundColor: "#1679AB",
+    marginTop: "110%",
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+  },
+  career: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  careerText: {
+    color: 'white',
+  },
+  social: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  socialText: {
+    color: 'white',
+  },
+  finance: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  financeText: {
+    color: 'white',
+  },
+  activities: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  activitiesText: {
+    color: 'white',
+  },
+  allOptionsButton: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  allOptionsText: {
+    color: 'white',
+  },
+  menuContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+    height: height / 2,
+  },
+  menuContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+  },
+  menuOptionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  menuOptionText: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
 });
 
 export default Aspect;
