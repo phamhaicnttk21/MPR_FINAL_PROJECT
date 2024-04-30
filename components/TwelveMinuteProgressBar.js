3113c9c0756dc679a96f36762b0cbc3a3d99afc2
hothuyhien <hothuyhien2412@gmail.com>
2024-04-30 18:06:45 +0700

    fix progress bar

--- components/TwelveMinuteProgressBar.js
+++ components/TwelveMinuteProgressBar.js
@@ -9,18 +9,16 @@ const TwelveMinuteProgressBar = () => {
 
   useEffect(() => {
     const totalDuration = 720000; // 12 minutes in milliseconds
-
     const animate = (currentTime) => {
       if (!startTime.current) {
         startTime.current = currentTime;
       }
-
       const elapsedTime = currentTime - startTime.current;
       const newProgress = elapsedTime / totalDuration;
-
       if (newProgress >= 1) {
-        setProgress(1);
-        cancelAnimationFrame(requestRef.current);
+        setProgress(0); // Reset progress to 0
+        startTime.current = null; // Reset startTime to restart the timer
+        requestRef.current = requestAnimationFrame(animate);
       } else {
         setProgress(newProgress);
         requestRef.current = requestAnimationFrame(animate);
